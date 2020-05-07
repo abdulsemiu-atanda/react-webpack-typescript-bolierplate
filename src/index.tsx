c357d2df49e9dfc50e936650941b4ab1d5c41bdd
Abdulsemiu Atanda <semiu@live.com>
2020-05-07 15:08:27 -0400

    react components with typescript

---
+++ src/index.tsx
@@ -0,0 +1,10 @@
+import * as React from 'react'
+import * as ReactDOM from 'react-dom'
+
+import App from './components/App'
+
+import './styles/styles.scss'
+
+const Root = document.getElementById('root')
+
+ReactDOM.render(<App name='Segun' />, Root)
